cfe4e7f53d6a4d71d440d87b858b995c3ba46bca
Jericho <joshhager04@gmail.com>
2017-07-12 10:51:11 -0400

    Update flappy.js

--- flappy.js
+++ flappy.js
@@ -19,7 +19,7 @@ var CONFIG = {
       "gravity": 1200,
       "outkill": true,
       "flap": {
-        "velocity": -225,
+        "velocity": -185,
         "frame": 0,
         "rotate": -40,
         "norotate": true
